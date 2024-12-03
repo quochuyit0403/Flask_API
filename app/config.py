a202e42cd45bd9840ad6ee32f3fc1704dcde9ace
quochuy43 <kuhuylevan@gmail.com>
2024-12-03 16:05:00 +0700

    update

--- app/config.py
+++ app/config.py
@@ -2,6 +2,11 @@ import os
 
 class Config:
     # Cấu hình SQLite cho Heroku
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///database.db')  # Lấy từ môi trường Heroku
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///database.db')  # Lấy từ môi trường Heroku
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SECRET_KEY = os.urandom(24)
+
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Đường dẫn tuyệt đối tới thư mục chứa config.py
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
